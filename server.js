9151fa5454ed57f231094565a6f9cb45e23683af
Daniel Loran <lor20011@byui.edu>
2023-06-07 15:44:48 -0600

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-new change test
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Jesus Ramirez");
+});
+
+const port = 3000;
+ 
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
